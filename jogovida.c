91ed67f73a9740263471c25583f441071a4d3562
Lucas <lcdsdlima@inf.ufpel.edu.br>
2015-04-30 22:44:12 -0300

    pthreads

--- jogovida.c
+++ jogovida.c
@@ -1,20 +1,36 @@
 #include<stdio.h>
+#include<pthread.h>
+#include<math.h>
 
-<<<<<<< HEAD
-int** iteracao(int** mat, int tam, int linha){
-	int i, j, x,contador=0,contador2=0,**mat2;
+#define nthreads 2
+
+typedef struct{
+	int** master;
+	int tamanho;
+	int line;
+	int minimo;
+	int maximo;
+} parametros;
+
+void* iteracao(void* teste){
+	int i, j, x,contador=0,contador2=0,**mat2,**mat,tam,linha,imin,imax;
+	parametros* outro = (parametros*)teste;
+	mat=outro->master;
+	tam=outro->tamanho;
+	linha=outro->line;
+	imin=outro->minimo;
+	imax=outro->maximo;
 	x = tam;
 	mat2 = (malloc( x * sizeof(int)));
     for (j = 0; j < x; j++){
         mat2[j] = (malloc(x * sizeof(int)));
     }
-    printf("\n \n \n");
     for(i=0;i<x;i++){
 		for(j=0;j<x;j++){
 			mat2[i][j]=mat[i][j];	
 		}
 	}
-	for(i = 0; i < x; i++){
+	for(i = imin; i < imax; i++){
        		printf("\n");
        		for(j = 0; j < x; j++){
             	if(i==0 && j==0){
@@ -336,22 +352,26 @@ int** iteracao(int** mat, int tam, int linha){
 			contador2=0;
         	}
     	}
-    	return mat2;
+    	pthread_exit(mat2);
 }
 
 main(){
-    int i,j,x, **matriz, temp1,temp2,temp3,iteracoes=10,i2;
+    int i,j,x, **matriz, temp1,temp2,temp3,iteracoes=10000,i2,minimo,maximo,imin,imax,resto=0;
+    float tamlinha;
     FILE * fp;
+    parametros teste;
+    parametros teste2;
+    pthread_t t1;
+    pthread_t t2;
     x = 10;
+    tamlinha=x/nthreads;
+    resto = x%nthreads;
+    minimo=tamlinha;
+    
+    printf("minimo : %d \n",minimo);
+    maximo=tamlinha+resto;
+    printf("maximo : %d \n",maximo);
     printf("%d %d \n", x, x);
-=======
-main(){
-    int y,i,j,x, **matriz, temp1,temp2,temp3;
-    FILE * fp;
-    scanf("%d", &x);
-    scanf("%d", &y);
-    printf("%d %d \n", x, y);
->>>>>>> ad31163ba16f6c2ffcb8a7652ca85e5787700c91
 
 	fp=fopen("entrada.txt","r");
 	if (!fp){
@@ -361,20 +381,12 @@ main(){
 
     matriz = (malloc( x * sizeof(int)));
     for (j = 0; j < x; j++){
-<<<<<<< HEAD
         matriz[j] = (malloc(x * sizeof(int)));
-=======
-        matriz[j] = (malloc(y * sizeof(int)));
->>>>>>> ad31163ba16f6c2ffcb8a7652ca85e5787700c91
     }
 
     //Zerando matriz inicial
     for(i = 0; i < x; i++){
-<<<<<<< HEAD
         for(j = 0; j < x; j++){
-=======
-        for(j = 0; j < y; j++){
->>>>>>> ad31163ba16f6c2ffcb8a7652ca85e5787700c91
             matriz[i][j] = 0;
         }
     }
@@ -383,37 +395,51 @@ main(){
         fscanf(fp, "%d", &temp2);
         fscanf(fp, "%d", &temp3);
         matriz[temp1-1][temp2-1] = temp3;
-<<<<<<< HEAD
         printf("L: %d C: %d Celula: %d\n", temp1, temp2, temp3);
-=======
-        printf("teste %d %d %d teste\n", temp1, temp2, temp3);
->>>>>>> ad31163ba16f6c2ffcb8a7652ca85e5787700c91
     }
     fclose(fp);
     // Testar a matriz
     for(i = 0; i < x; i++){
         printf("\n");
-<<<<<<< HEAD
         for(j = 0; j < x; j++){
             printf("%d ", matriz[i][j]);
         }
     }
+    teste.master=matriz;
+    teste.tamanho=x;
+    teste.line=1;
+    teste.minimo=0;
+    teste.maximo=maximo;
+    teste2.master=matriz;
+    teste2.tamanho=x;
+    teste2.line=1;
+    teste2.minimo=maximo+1;
+    teste2.maximo=minimo+maximo;
     for(i2=0;i2<iteracoes;i2++){
-	matriz=iteracao(matriz, x, 1);
+	pthread_create (&t1,NULL,iteracao,(void *) &teste);
+	pthread_create (&t2,NULL,iteracao,(void *) &teste2);
+	pthread_join(t1,(void **) &teste.master);
+	pthread_join(t2,(void **) &teste2.master);
+	for(i=0;i<maximo;i++){
+		for(j=0;j<x;j++){
+			matriz[i][j]=teste.master[i][j];
+		}
+	}
+	for(i=maximo+1;i<maximo+minimo;i++){
+		for(j=0;j<x;j++){
+			matriz[i][j]=teste2.master[i][j];
+		}
+	}
+	teste.master=matriz;
+	teste2.master=matriz;
 	for(i = 0; i < x; i++){
         printf("\n");
         for(j = 0; j < x; j++){
-=======
-        for(j = 0; j < y; j++){
->>>>>>> ad31163ba16f6c2ffcb8a7652ca85e5787700c91
-            printf("%d ", matriz[i][j]);
+            printf("%d ", teste.master[i][j]);
         }
     }
 }
-<<<<<<< HEAD
 }
 
-=======
->>>>>>> ad31163ba16f6c2ffcb8a7652ca85e5787700c91
 
 
